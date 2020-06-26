6678dd0e2cbfe49e2bcbc6609fef64d365631ce7
Jayneel <jayneel9472@gmail.com>
2020-06-26 16:07:26 +0530

    some fragments

--- app/src/main/java/com/jayneel/socialmedia/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/jayneel/socialmedia/Fragment/ProfileFragment.kt
@@ -1,12 +1,23 @@
 package com.jayneel.socialmedia.Fragment
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.jayneel.socialmedia.Model.userModel
 import com.jayneel.socialmedia.R
+import kotlinx.android.synthetic.main.profile_fragment.*
+import kotlinx.android.synthetic.main.profile_fragment.view.*
+
 
 class ProfileFragment : Fragment() {
 
@@ -27,6 +38,17 @@ class ProfileFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
         // TODO: Use the ViewModel
+        var user=FirebaseAuth.getInstance().currentUser?.uid
+
+        viewModel.getdata(user!!)?.observe(viewLifecycleOwner, Observer {
+            progressBar2.visibility=View.VISIBLE
+            profile_full_name.setText(it[0].name)
+            progressBar2.visibility=View.INVISIBLE
+
+        })
+
+               }
+
+
     }
 
-}
--- app/src/main/java/com/jayneel/socialmedia/Fragment/ProfileViewModel.kt
+++ app/src/main/java/com/jayneel/socialmedia/Fragment/ProfileViewModel.kt
@@ -1,35 +1,52 @@
 package com.jayneel.socialmedia.Fragment
 
+import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.jayneel.socialmedia.Model.userModel
+import kotlinx.android.synthetic.main.profile_fragment.*
 
 class ProfileViewModel : ViewModel() {
 
-    lateinit var profiledata:LiveData<List<userModel>>
+    lateinit var profiledata: MutableLiveData<List<userModel>>
 
-    fun getdata(uid:String){
-        var myref=FirebaseDatabase.getInstance().getReference("user")
-        myref.child(uid).addValueEventListener(object :ValueEventListener{
+    fun getdata(uid: String): MutableLiveData<List<userModel>>? {
+        var data = MutableLiveData<List<userModel>>()
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("user")
+
+        // Read from the database
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated
+                var list=ArrayList<userModel>()
+                for (d in dataSnapshot.children) {
+                    val value = d.getValue(userModel::class.java)!!
+                    list.add(value)
+                    data.value=list
+                    Log.d("TAG", "Value is: $value")
+                }
+
+            }
+
             override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException())
             }
-            var data=ArrayList<userModel>()
-            override fun onDataChange(snapshot: DataSnapshot) {
-              for(ds in snapshot.children)
-              {
-
-              }
-            }
-
         })
+        return data
+
     }
-
-
 }
+
 
 
