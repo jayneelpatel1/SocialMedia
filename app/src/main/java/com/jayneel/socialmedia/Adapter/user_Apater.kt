c506a484489956d849837c827a026818274f1b1c
Jayneel <jayneel9472@gmail.com>
2020-06-30 14:09:49 +0530

    Follow and unfollow done

--- app/src/main/java/com/jayneel/socialmedia/Adapter/user_Apater.kt
+++ app/src/main/java/com/jayneel/socialmedia/Adapter/user_Apater.kt
@@ -6,11 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.jayneel.socialmedia.Model.userModel
 import com.jayneel.socialmedia.R
 import kotlinx.android.synthetic.main.user_iteam_layout.view.*
 
 class user_Apater(var ctx:Context,var list: ArrayList<userModel>,var isFragment:Boolean=false):RecyclerView.Adapter<user_Apater.viewholder>() {
+    var FirebaseUser=FirebaseAuth.getInstance().currentUser
+    var myRef=FirebaseDatabase.getInstance().getReference("Follow")
     inner class viewholder(v:View):RecyclerView.ViewHolder(v){
         var usernmae=v.user_iteam_text
         var img=v.user_iteam_img
@@ -28,7 +37,51 @@ class user_Apater(var ctx:Context,var list: ArrayList<userModel>,var isFragment:
     }
 
     override fun onBindViewHolder(holder: viewholder, position: Int) {
+
         holder.usernmae.setText(list[position].username)
         holder.email.setText(list[position].email)
+        holder.btn.setOnClickListener {
+            chkfollowingstatus(list[position].uid.toString(),holder.btn)
+            if(holder.btn.text=="Follow"){
+                myRef.child(FirebaseUser!!.uid).child("Following").child(list[position].uid.toString()).setValue(true)
+                    .addOnCompleteListener {task->
+                        if(task.isSuccessful){
+                            myRef.child(list[position].uid.toString()).child("Followers").child(
+                                FirebaseUser!!.uid).setValue(true)
+                        }
+                    }
+            }
+            else
+            {
+                myRef.child(FirebaseUser!!.uid).child("Following").child(list[position].uid.toString()).removeValue()
+                    .addOnCompleteListener {task->
+                        if(task.isSuccessful){
+                            myRef.child(list[position].uid.toString()).child("Followers").child(FirebaseUser!!.uid).removeValue()
+                        }
+                    }
+            }
+        }
     }
+
+    private fun chkfollowingstatus(uid: String, btn: MaterialButton?) {
+        var followingref=  myRef.child(FirebaseUser!!.uid).child("Following")
+        followingref.addValueEventListener(object :ValueEventListener{
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.child(uid).exists()){
+                    btn!!.text="UnFollow"
+                }
+                else
+                {
+                    btn!!.text="Follow"
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+
+        })
+
+    }
+
 }
