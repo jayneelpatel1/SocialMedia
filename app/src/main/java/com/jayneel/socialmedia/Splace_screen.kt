e870753a3d6bf2329bcff4247dff8a94518c15fb
AmanTailor21 <amantailor21@gmail.com>
2020-07-07 20:36:01 +0530

    splace Screen

---
+++ app/src/main/java/com/jayneel/socialmedia/Splace_screen.kt
@@ -0,0 +1,18 @@
+package com.jayneel.socialmedia
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splace_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splace_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,login::class.java))
+            finish()
+        },3000)
+        }
+    }
