0ffaa1d6826e890c663cfe2c790f9772b8c7f5b6
Jayneel <jayneel9472@gmail.com>
2020-07-16 14:54:42 +0530

    message done only left and right disply code need to check

---
+++ app/src/main/java/com/jayneel/socialmedia/Adapter/msgAdapter.kt
@@ -0,0 +1,51 @@
+package com.jayneel.socialmedia.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.jayneel.socialmedia.Model.MsgModel
+import com.jayneel.socialmedia.R
+import kotlinx.android.synthetic.main.msgleft.view.*
+import kotlinx.android.synthetic.main.msgright.view.*
+import org.w3c.dom.Text
+
+class msgAdapter(var mconttxt:Context,var msg:ArrayList<MsgModel>):RecyclerView.Adapter<msgAdapter.viewholder>()
+{
+    var firebaseUser=FirebaseAuth.getInstance().currentUser!!.uid
+    inner class viewholder(v:View):RecyclerView.ViewHolder(v){
+        var msg=v.findViewById<TextView>(R.id.show_txt_msg)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): viewholder {
+        return if (position==1){
+            var view=LayoutInflater.from(mconttxt).inflate(R.layout.msgleft,parent,false)
+            viewholder(view)
+        }
+        else{
+            var view=LayoutInflater.from(mconttxt).inflate(R.layout.msgright,parent,false)
+            viewholder(view)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return super.getItemViewType(position)
+        return if(msg[position].sender.equals(firebaseUser))
+        {
+            1
+        }
+        else
+            0
+    }
+    override fun getItemCount(): Int {
+      return msg.size
+    }
+
+    override fun onBindViewHolder(holder: viewholder, position: Int) {
+        var chat=msg[position]
+       holder.msg.text=chat.msg
+    }
+}
--- app/src/main/java/com/jayneel/socialmedia/message.kt
+++ app/src/main/java/com/jayneel/socialmedia/message.kt
@@ -2,8 +2,14 @@ package com.jayneel.socialmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.jayneel.socialmedia.Adapter.msgAdapter
 import com.jayneel.socialmedia.Model.MsgModel
 import kotlinx.android.synthetic.main.activity_message.*
 
@@ -15,6 +21,12 @@ class message : AppCompatActivity() {
         setContentView(R.layout.activity_message)
 
         var uid=intent.getStringExtra("uid")
+
+        retrivemsg(firebaseUser,uid)
+        msgrv.setHasFixedSize(true)
+        var layoutManager=LinearLayoutManager(applicationContext)
+        layoutManager.stackFromEnd=true
+        msgrv.layoutManager=layoutManager
 
         btnsend.setOnClickListener {
             val msg=edtmsgtext.text.toString()
@@ -28,6 +40,36 @@ class message : AppCompatActivity() {
             }
             edtmsgtext.text.clear()
         }
+    }
+
+    private fun retrivemsg(sender: String, reciver: String?) {
+        var myref=FirebaseDatabase.getInstance().getReference("message")
+        myref.addValueEventListener(object :ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var arlist=ArrayList<MsgModel>()
+                var ad=msgAdapter(this@message,arlist)
+                for(key in snapshot.children){
+                    var value=key.getValue(MsgModel::class.java)
+                    if (value != null) {
+                        if(value.sender.equals(sender) && value.reciver.equals(reciver)
+                            || value.sender.equals(reciver) && value.reciver.equals(sender))
+                            {
+                            arlist.add(value)
+
+                        }
+                    }
+
+                }
+                msgrv.adapter=ad
+
+
+            }
+
+        })
     }
 
     private fun sendmsgtouser(reciver: String?, sender: String, msg: String) {
