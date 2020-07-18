fc05986f3527675e43a13f1e538cbde8fae9e30d
Jayneel <jayneel9472@gmail.com>
2020-07-18 17:34:26 +0530

    message list

--- app/src/main/java/com/jayneel/socialmedia/Adapter/chatlistAdapter.kt
+++ app/src/main/java/com/jayneel/socialmedia/Adapter/chatlistAdapter.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
 import com.jayneel.socialmedia.Model.chatlistModel
 import com.jayneel.socialmedia.Model.userModel
 import com.jayneel.socialmedia.R
@@ -29,6 +31,14 @@ class chatlistAdapter(var ctx:Activity,var arlist:ArrayList<userModel>):Recycler
     }
 
     override fun onBindViewHolder(holder: viewholder, position: Int) {
+        if(arlist[position].img!="")
+        {
+            val storage = FirebaseStorage.getInstance()
+            val storageReference = storage.getReferenceFromUrl(arlist[position].img!!)
+            storageReference.downloadUrl.addOnSuccessListener {
+                Glide.with(ctx).load(it.toString()).into(holder.pic).view
+            }
+        }
         holder.usernmae.text=arlist[position].username.toString()
         holder.itemView.setOnClickListener {
             var int1= Intent(ctx, message::class.java)
